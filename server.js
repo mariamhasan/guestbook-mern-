30f2ac3e5e700aff1c887a1434fa3569a22436b8
mariam <mariem.hasan@yahoo.com>
2020-09-17 13:54:00 -0700

    implementing register in users route

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+//database connection
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  err => {
+    if (err) return console.error(err);
+    console.log("connection established");
+  }
+);
+
+//setup routes
+app.use("/users", require("./routes/users"));
